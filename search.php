0b573c8ac9876a81124bf0307efa5d8e3d61bb95
Vio <vio@dramaforum.ca>
2016-03-31 12:53:37 -0700

    Updated logging in search

--- search.php
+++ search.php
@@ -4,11 +4,12 @@ require 'DAL.php';
 
 // get input either from POST or GET (GET only used for easy testing)
 $input_data = ($_GET) ? $_GET['json'] : file_get_contents('php://input');
-error_log("User data loaded to search : \n $input_data");
+error_log("Input data loaded to search : \n $input_data");
 if ($input_data) {
     $input_data = json_decode($input_data);
     // convert stdClass => array
     $user_data = json_decode(json_encode($input_data), true);
+	error_log("User data loaded to search : \n $user_data");
     $dal = new DAL();
     $results = $dal->get_campaigns($user_data['profile']);
     echo json_encode($results, JSON_PRETTY_PRINT);
