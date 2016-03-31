d0f7fa343373346f89d2cea1a5071a0f55538530
Vio <vio@dramaforum.ca>
2016-03-31 13:14:33 -0700

    Added .htacess in hope to fix encoding

--- search.php
+++ search.php
@@ -11,6 +11,6 @@ if ($input_data) {
     $user_data = json_decode(json_encode($input_data), true);
 	error_log("User data loaded to search : \n $user_data");
     $dal = new DAL();
-    $results = $dal->get_campaigns($user_data->profile);
+    $results = $dal->get_campaigns($user_data['profile']);
     echo json_encode($results, JSON_PRETTY_PRINT);
 }
