ebf64a4388fc5d1bd5bab9fbcf426c365e473c07
vio <vio@dev.dev>
2016-04-01 16:19:25 +0300

    Updated auto search function (maybe it was encoding json wrong)

--- search.php
+++ search.php
@@ -6,14 +6,12 @@ require 'DAL.php';
 $input_data = ($_GET) ? $_GET['json'] : file_get_contents('php://input');
 error_log("Input data loaded to search : \n $input_data");
 if ($input_data) {
-    //$input_data = json_decode($input_data, true);
-    // convert stdClass => array
-    //$user_data = json_decode(json_encode($input_data), true);
 	$user_data = json_decode($input_data, true);
-	error_log("User data loaded to search : \n $user_data");
-	//$user_attrs = json_decode($user_data, true);
-	//error_log("User attrs : " +  json_encode($user_attrs));
+	//error_log("User data loaded to search : \n $user_data");
+	// on heroku we need double decoding (probably some environment misconfiguration which we cannot override)
+	$user_attrs = json_decode($user_data, true);
+	error_log("User attrs : " +  json_encode($user_attrs));
     $dal = new DAL();
-    $results = $dal->get_campaigns($user_data['profile']);
+    $results = $dal->get_campaigns($user_attrs['profile']);
     echo json_encode($results, JSON_PRETTY_PRINT);
 }
