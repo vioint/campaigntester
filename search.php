02db5e877bdd0ab39ce80c1e303427428e6ee2fc
vio <vio@dev.dev>
2016-04-01 12:49:05 +0300

    Updated auto search function (maybe it was double encoding json at some other point)

--- search.php
+++ search.php
@@ -6,9 +6,9 @@ require 'DAL.php';
 $input_data = ($_GET) ? $_GET['json'] : file_get_contents('php://input');
 error_log("Input data loaded to search : \n $input_data");
 if ($input_data) {
-    $input_data = json_decode($input_data);
+    //$input_data = json_decode($input_data);
     // convert stdClass => array
-    $user_data = json_decode(json_encode($input_data), true);
+    $user_data = json_decode($input_data, true);
 	error_log("User data loaded to search : \n $user_data");
     $dal = new DAL();
     $results = $dal->get_campaigns($user_data['profile']);
